e95b6366af19087755d69b0eccb6bb94cb91005a
trg-nickr <nicholas.ricci@techresearchgroup.com>
2022-11-15 10:58:04 -0500

    cleaning up some code

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 """MTA TO ONENOTE CONVERTER"""
-from plistlib import InvalidFileException
-from tkinter import Button, Entry, filedialog, Frame, Label, messagebox, Text
-from tkinter.constants import END, FALSE
+import contextlib
 import tkinter as tk
 import tkinter.font as tkfont
+from plistlib import InvalidFileException
+from tkinter import Button, Entry, Frame, Label, Text, filedialog, messagebox
+from tkinter.constants import END, FALSE
+
 import openpyxl
 
 ICON = r"C:\\Users\\nicho\\Desktop\\Dev Projects\\MTA Converter\\logo_TRG.ico"
-    
+
+
 def open_file():
     """Using openpyxl to read excel file."""
 
-     # Enable convert button
+    # Enable convert button
     btn_convert.configure(command=lambda: convert_file(path))
 
     # Open file and get path
@@ -20,11 +23,12 @@ def open_file():
         ('all files', '*.*')), defaultextension=".xlsx")
 
     if path != "":
-        print("Excel file located at: " + path)
+        print(f"Excel file located at: {path}")
 
         return path
     else:
         messagebox.showerror("ERROR", "Please select a valid file.")
+
 
 def convert_file(path):
     """Converts MTA spreadsheet to OneNote style documentation."""
@@ -42,7 +46,7 @@ def convert_file(path):
 
     # Clear the text box
     txt_output.delete(1.0, END)
-    
+
     try:
         # Get row number from user
         _wp = int(txt_row.get())
@@ -50,24 +54,26 @@ def convert_file(path):
         # Load excel with its path
         wrkbk = openpyxl.load_workbook(path)
         _sh = wrkbk.active
-        
+
         # Check if the row number is valid
         if _wp > _sh.max_row:
             messagebox.showerror("ERROR", "Please enter a valid row number.")
         else:
-            txt_output.insert("1.0", "Initial Setup Box:\n\nTest Equipment:\n\n")
+            txt_output.insert(
+                "1.0", "Initial Setup Box:\n\nTest Equipment:\n\n")
             # Iterate through excel and display data
             for j in range(1, _sh.max_column + 1):
                 headers = _sh.cell(row=2, column=j).value
-                cell_obj = _sh.cell(row = _wp, column = j)
-                cells = str(cell_obj.value).split("\t")        
+                cell_obj = _sh.cell(row=_wp, column=j)
+                cells = str(cell_obj.value).split("\t")
                 for j in cells:
-                    if j != "None" and j != "N/A" and j != "n/a" and j != "":
-                    # if j not in ["None", "N/A", ""]:
+                    # if j != "None" and j != "N/A" and j != "n/a" and j != "":
+                    if j not in ["None", "N/A", "n/a", ""]:
+                        # if j not in ["None", "N/A", ""]:
                         # if headers == isb[0]:   # Test Equipment
-                            # txt_output.insert(END, "Test Equipment:\n")
-                            # txt_output.insert(END, j + "\n\n")
-                        if headers == isb[1]: # Tools
+                        # txt_output.insert(END, "Test Equipment:\n")
+                        # txt_output.insert(END, j + "\n\n")
+                        if headers == isb[1]:  # Tools
                             tools = j.split(",")
                             txt_output.insert(END, headers + ":\n")
                             for tool in tools:
@@ -76,29 +82,33 @@ def convert_file(path):
                                 else:
                                     txt_output.insert(END, tool + "\n")
                             txt_output.insert(END, "\nMaterials:\n")
-                        elif headers == "Exp/Dur":# Materials
+                        elif headers == "Exp/Dur":  # Materials
                             txt_output.insert(END, j + "\n")
-                        elif headers == "Replacement Parts": # Materials
+                        elif headers == "Replacement Parts":  # Materials
                             txt_output.insert(END, j + "\n")
-                        elif headers == isb[3]: # MRP
-                            txt_output.insert(END, "\nMaterial Replacement Parts:\n")
+                        elif headers == isb[3]:  # MRP
+                            txt_output.insert(
+                                END, "\nMaterial Replacement Parts:\n")
                             txt_output.insert(END, j + "\n")
-                        elif headers == isb[4]: # Personnel
+                        elif headers == isb[4]:  # Personnel
                             txt_output.insert(END, "\nPersonnel:\n")
-                            txt_output.insert(END, headers + ": " + j + "\n")
+                            txt_output.insert(END, f"{headers} : {j}\n")
                         elif headers == "Personnel Required":
                             if int(j) >= 2:
                                 txt_output.insert(END, j + " people\n\n")
                                 txt_output.insert(END, "References:\n\n")
-                                txt_output.insert(END, "Equipment Description:\n\n")
+                                txt_output.insert(
+                                    END, "Equipment Description:\n\n")
                             else:
                                 txt_output.insert(END, j + " person\n\n")
                                 txt_output.insert(END, "References:\n\n")
-                                txt_output.insert(END, "Equipment Description:\n\n")
+                                txt_output.insert(
+                                    END, "Equipment Description:\n\n")
     except InvalidFileException:
         messagebox.showerror("ERROR", "Please select a valid file.")
     except ValueError:
         messagebox.showerror("ERROR", "Please enter a valid row number.")
+
 
 def save_file():
     """Save the converted file to a text file."""
@@ -106,22 +116,20 @@ def save_file():
         onenote = txt_output.get(1.0, END)
         filename = filedialog.asksaveasfilename(initialdir="/", title="Save as", filetypes=(
             ('txt files', '*.txt'), ('all files', '*.*')), defaultextension=".txt")
-        print("File saved at: " + filename)
+        print(f"File saved at: {filename}")
         with open(filename, "w", encoding="utf-8") as file:
             file.write(onenote)
     except FileNotFoundError:
         messagebox.showerror("ERROR", "File not saved.")
 
+
 root = tk.Tk()
 root.title("MTA Converter")
-root.geometry('{}x{}'.format(500, 920))
+root.geometry('500x920')
 root.resizable(width=FALSE, height=FALSE)
 
-try:
+with contextlib.suppress(tk.TclError):
     root.iconbitmap(ICON)
-except tk.TclError:
-    pass
-
 # Create a Frame
 frame1 = Frame(root)
 frame1.grid(row=0, columnspan=4)
@@ -129,25 +137,26 @@ frame1.grid(row=0, columnspan=4)
 lbl_row = Label(frame1, text="WP Row:", font='Helvetica 12 bold')
 lbl_row.grid(row=0, column=0, padx=10, pady=20)
 
-txt_row = Entry(frame1, width=10, font='Helvetica 12', bg="#FFFFFF", fg="#000000")
+txt_row = Entry(frame1, width=10, font='Helvetica 12',
+                bg="#FFFFFF", fg="#000000")
 txt_row.grid(row=0, column=1, padx=10, pady=20)
 
 btn_open = Button(frame1, text="Open File", command=open_file, width=10,
-    font='Helvetica 12 bold', bg="blue", fg="white")
+                  font='Helvetica 12 bold', bg="blue", fg="white")
 btn_open.grid(row=0, column=2, padx=10, pady=20)
 
 btn_convert = Button(frame1, text="Convert", width=10,
-        font='Helvetica 12 bold', bg="blue", fg="white")
+                     font='Helvetica 12 bold', bg="blue", fg="white")
 btn_convert.grid(row=0, column=3, padx=10, pady=20)
 
 txt_output = Text(frame1, font='Menlo 12', height=44, width=53)
 txt_output.grid(row=1, columnspan=4, padx=10)
 font = tkfont.Font(font=txt_output['font'])
-tab=font.measure("    ")
+tab = font.measure("    ")
 txt_output.configure(tabs=tab)
 
 btn_save = Button(frame1, text="SAVE", command=save_file,
-    font='Helvetica 12 bold', width=45, bg="blue", fg="white")
+                  font='Helvetica 12 bold', width=45, bg="blue", fg="white")
 btn_save.grid(row=2, columnspan=4, padx=10, pady=10)
 
 
