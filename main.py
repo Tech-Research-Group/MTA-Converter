18b3addf92b2ca475e3313f2477107d59f931091
trg-nickr <nicholas.ricci@techresearchgroup.com>
2023-01-19 16:37:05 -0500

    Fixed material parts bug, added task description -> steps, and fixed parsed MOS/personnel required data

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import tkinter.font as tkfont
 from plistlib import InvalidFileException
 from tkinter import Button, Entry, Frame, Label, Text, filedialog, messagebox
 from tkinter.constants import END, FALSE
-
 import openpyxl
+
 
 ICON = r"C:\\Users\\nicho\\Desktop\\Dev Projects\\MTA Converter\\logo_TRG.ico"
 # List containing all the ISB titles
@@ -43,8 +43,8 @@ def convert_file(path):
     """Converts MTA spreadsheet to OneNote style documentation."""
     # Clear the text box
     txt_output.delete(1.0, END)
-    global materials
-    materials = []
+    # global materials
+    # materials = []
 
     try:
         # Get row number from user
@@ -70,67 +70,122 @@ def convert_file(path):
                 cells = str(cell_obj.value).split("\t")
 
                 for col in cells:
-                    if col not in ["None", "N/A", "n/a", ""]:
-                        if headers == "Component Name":
-                            txt_output.insert(END, f"{col} (")
-                        if headers == "Maintainer Task":
-                            txt_output.insert(END, f"{col})\n\n")
-                            txt_output.insert(END, "WPID: \n\n")
-                        
-                        if headers == isb[0]:   # Test Equipment
+                    if headers == "Component Name":
+                        txt_output.insert(END, f"{col} (")
+                    if headers == "Maintainer Task":
+                        txt_output.insert(END, f"{col})\n\n")
+                        txt_output.insert(END, "WPID: \n\n")                 
+                    if headers == isb[0]:   # Test Equipment
+                        if col not in ["None", "N/A", "n/a", ""]:
                             txt_output.insert(END, "Test Equipment:\n")
                             txt_output.insert(END, col + "\n\n")
+                        else:
+                            txt_output.insert(END, "Test Equipment:\n\n")
+                    if headers == isb[1]:  # Tools
+                        txt_output.insert(END, headers + ":\n")
+                        tools = col.split("\n")
                         
-                        if headers == isb[1]:  # Tools
-                            txt_output.insert(END, headers + ":\n")
-                            tools = col.split("\n")
-                            
-                            for tool in tools: # was tool_list
-                                if "GMTK" in tool:
-                                    txt_output.insert(END, "Tool Kit, General Mechanic's\n")
-                                if "SATS" in tool:
-                                    sats_tool = tool.split("(")
-                                    txt_output.insert(END, sats_tool[0][:-1] + " (PART OF SATS)\n")
-                                if "GMTK" not in tool and "SATS" not in tool:
-                                    txt_output.insert(END, tool + "\n")
-                            txt_output.insert(END, "\nMaterials:\n")
+                        for tool in tools: # was tool_list
+                            if "GMTK" in tool:
+                                txt_output.insert(END, "Tool Kit, General Mechanic&apos;s\n")
+                            if "SATS" in tool:
+                                sats_tool = tool.split("(")
+                                txt_output.insert(END, sats_tool[0][:-1] + " (SATS)\n")
+                            if "GMTK" not in tool and "SATS" not in tool:
+                                txt_output.insert(END, tool + "\n")
+                        txt_output.insert(END, "\nMaterials:\n")
 
-                        if headers == "Replacement Parts":
-                            if col not in ["None", "N/A", "n/a", ""]:
-                                parts = col.split("\n")
-                                materials = list(parts)
-                        if headers == "Exp/Dur":
-                            if col not in ["None", "N/A", "n/a", ""]:
-                                expendables = col.split("\n")
+                    if headers == "Replacement Parts":
+                        global materials
+                        materials = []
+                        if col not in ["None", "N/A", "n/a", ""]:
+                            parts = col.split("\n")
+                            for part in parts:
+                                materials.append(part)
+                                txt_output.insert(END, part + "\n")
+                        else:
+                            materials = []
+                        # print(materials)
+                    if headers == "Exp/Dur":
+                        if col not in ["None", "N/A", "n/a", ""]:
+                            expendables = col.split("\n")
                             for expendable in expendables:
                                 materials.append(expendable)
-                            for material in materials:
-                                txt_output.insert(END, material + "\n")
-                        
-                        if headers == isb[3]:  # MRP
-                            txt_output.insert(
-                                END, "\nMandatory Replacement Parts:\n")
+                                txt_output.insert(END, expendable + "\n")
+                    if headers == isb[3]:  # MRP
+                        txt_output.insert(
+                            END, "\nMandatory Replacement Parts:\n")
+                        if col not in ["None", "N/A", "n/a", ""]:
                             mrp_list = col.split("\n")
                             for mrp in mrp_list:
                                 txt_output.insert(END, mrp + "\n")
+                    
+                    if headers == isb[4]:  # Personnel
+                        global personnel
+                        personnel = 0
+                        txt_output.insert(END, "\nPersonnel:\n")
+                        # if (col != "MOS Non-specific"):
+                        if "MOS" not in col:
+                            txt_output.insert(END, f"{headers}: {col}")
+                            personnel = 1
+                        else:
+                            personnel = 0
                         
-                        if headers == isb[4]:  # Personnel
-                            txt_output.insert(END, "\nPersonnel:\n")
-                            txt_output.insert(END, f"{headers} : {col}\n")
-                        if headers == "Personnel Required":
+                    if headers == "Personnel Required":
+                        if personnel == 1:
+                            personnel = int(col) - personnel
+                            print(personnel)
+                            if personnel > 1:
+                                txt_output.insert(END, "\n" + str(personnel) + " people\n\n")
+                                txt_output.insert(END, "References:\n\n")
+                                txt_output.insert(
+                                    END, "Equipment Condition:\n\n")
+                            elif personnel == 1:
+                                txt_output.insert(END, "\n" + str(personnel) + " person\n\n")
+                                txt_output.insert(END, "References:\n\n", END, "Equipment Condition:\n\n")
+                            elif personnel == 0:
+                                txt_output.insert(END, "\n\nReferences:\n\n", END, "Equipment Condition:\n\n")
+                        elif personnel == 0:
                             if int(col) >= 2:
                                 txt_output.insert(END, col + " people\n\n")
                                 txt_output.insert(END, "References:\n\n")
                                 txt_output.insert(
-                                    END, "Equipment Description:\n\n")
+                                    END, "Equipment Condition:\n\n")
                             else:
                                 txt_output.insert(END, col + " person\n\n")
-                                txt_output.insert(END, "References:\n\n", END, "Equipment Description:\n\n")
+                                txt_output.insert(END, "References:\n\n", END, "Equipment Condition:\n\n")
+            get_task_description(path)
+                    
+
     except InvalidFileException:
         messagebox.showerror("ERROR", "Please select a valid file.")
     except ValueError:
         messagebox.showerror("ERROR", "Please enter a valid row number.")
 
+def get_task_description(path):
+    """Selects the data from Task Description column and parses each task into a step."""
+    # Get row number from user
+    _wp = int(txt_row.get())
+
+    # # Load excel with its path
+    wrkbk = openpyxl.load_workbook(path)
+    _sh = wrkbk.active
+
+    # Iterate through excel and display data
+    for col in range(1, _sh.max_column + 1):
+        headers = _sh.cell(row=2, column=col).value
+        cell_obj = _sh.cell(row=_wp, column=col)
+        cells = str(cell_obj.value).split("\t")
+
+        for col in cells:
+            if headers == "Task Description":
+                txt_output.insert(END, "Maintenance Task Here:\n")
+                tasks = col.split("\n")
+                for task in tasks:
+                    if "***" not in task and "###" not in task:
+                        txt_output.insert(END, f".{task[:-1]}\n\n")
+                    else:
+                        txt_output.insert(END, f".{task[3:-3].upper()}: \n\n")
 
 def save_file():
     """Save the converted file to a text file."""
